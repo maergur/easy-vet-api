e1ffcbe89497c4a971cc4001e451057ecbaefbf6
maergur <153439629+maergur@users.noreply.github.com>
2024-06-09 23:03:17 +0300

    Web Config
    web config adjusted.

--- src/main/java/dev/patika/easyvet/config/WebConfig.java
+++ src/main/java/dev/patika/easyvet/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173") // Your frontend origin
+                .allowedOrigins("https://easyvet.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
