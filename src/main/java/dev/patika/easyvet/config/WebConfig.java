338c6a8b4bef2188e8d10a574ca6123d996c0904
maergur <153439629+maergur@users.noreply.github.com>
2024-06-16 21:10:36 +0300

    Fixed app properties.

--- src/main/java/dev/patika/easyvet/config/WebConfig.java
+++ src/main/java/dev/patika/easyvet/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedOrigins("http://easyvet.netlify.app")
+                .allowedOrigins("https://easyvet.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
